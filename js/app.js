1f22ac4598046d92560b4c6b9f7b033722f685c8
Martin Zalewski <martin.zalewski@gmail.com>
2018-09-29 14:47:18 +0200

    Zadanie nr 2 pokaż więcej, pokaż mniej

--- js/app.js
+++ js/app.js
@@ -16,4 +16,39 @@ var subMenu = document.querySelector('.dropdown');
     });
     
     
+var buttons = document.querySelectorAll('.read-more');
+    console.log(buttons);
+    
+console.log(buttons[0].previousElementSibling); // Powinien wyświetlić się w konsoli paragraf o klasie `more`
+
+    
+function showHide() {
+    
+var textArea = this.previousElementSibling;
+    
+    if (textArea.style.display === 'none' || textArea.style.display === ''){
+        
+        textArea.style.display = 'block';
+      this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+        
+    } else {
+        
+        textArea.style.display = 'none';
+        this.innerHTML = 'Więcej <span class="glyphicon glyphicon-chevron-down"></span>';
+    }
+    
+    
+}
+    
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', showHide);
+}    
+ 
+
+    
+    
+    
+
+    
+    
 });
