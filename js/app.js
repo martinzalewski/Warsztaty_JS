fdde523caec5becc562c85ca702a752509cd91c4
Martin Zalewski <martin.zalewski@gmail.com>
2018-09-29 12:51:09 +0200

    Zadanie nr 1

---
+++ js/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+      // tutaj będziemy umieszczać kod ze wszystkich zadań
+
+var menuButton = document.querySelector('.for-dropdown');
+    
+var subMenu = document.querySelector('.dropdown');
+    
+    menuButton.addEventListener('mouseover', function() {
+       subMenu.style.display = 'block';
+    });
+
+   //  menuButton.addEventListener('mouseout', function() {
+    //   subMenu.style.display = 'none';
+//    });
+    
+    
+});
